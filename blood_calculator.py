fbc894f557d174febd9261f71a119a61009e6701
Suraj U <suraj.upad@gmail.com>
2023-01-20 12:49:02 -0500

    added hdl driver and connected to interface

--- blood_calculator.py
+++ blood_calculator.py
@@ -3,12 +3,21 @@ def interface():
     keep_running = True
     while keep_running:
         print("Options:")
+        print("1: HDL")
         print("9 - Quit")
         choice = input("Enter your choice: ")
         if choice=='9':
             keep_running = False
+        elif choice == '1':
+            hdl_driver()
     print("program is done")
    
+    
+def hdl_driver():
+    hdl_in = hdl_input()
+    return hdl_anal(hdl_in)
+    
+    
 def hdl_input():
     hdl_val = input("enter the hdl result:")
     hdl_val = int(hdl_val)
