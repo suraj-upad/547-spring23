4558f395abc7f914fd936cea3a1089e1ec9fa73a
Suraj U <suraj.upad@gmail.com>
2023-01-20 14:25:46 -0500

    Added LDL option and driver, input, analysis, and ouput functions modeling hld functions

--- blood_calculator.py
+++ blood_calculator.py
@@ -4,12 +4,15 @@ def interface():
     while keep_running:
         print("Options:")
         print("1: HDL")
+        print("2: LDL")
         print("9 - Quit")
         choice = input("Enter your choice: ")
         if choice=='9':
             keep_running = False
         elif choice == '1':
             hdl_driver()
+        elif choice == '2':
+            ldl_driver()
     print("program is done")
    
     
@@ -17,7 +20,6 @@ def hdl_driver():
     hdl_in = hdl_input()
     hdl_anly= hdl_anal(hdl_in)
     hdl_output(hdl_in, hdl_anly)
-    
     
 def hdl_input():
     hdl_val = input("enter the hdl result:")
@@ -35,5 +37,31 @@ def hdl_anal(hdl_int):
 def hdl_output(hdl_val, hdl_analy):
     print("The HDL result of {} is considered {}".format(hdl_val, hdl_analy))
     return
+
+
+def ldl_driver():
+    ldl_in = ldl_input()
+    ldl_anly= ldl_anal(ldl_in)
+    ldl_output(ldl_in, ldl_anly)
+    
+def ldl_input():
+    ldl_val = input("enter the ldl result:")
+    ldl_val = int(ldl_val)
+    return ldl_val
+
+def ldl_anal(ldl_int):
+    if ldl_int >= 190:
+        return "Very High"
+    elif ldl_int>= 160:
+        return "High"
+    elif ldl_int >= 130:
+        return "Borderline High"
+    else:
+        return "Normal"
+    
+def ldl_output(ldl_val, ldl_analy):
+    print("The LDL result of {} is considered {}".format(ldl_val, ldl_analy))
+    return
+
 
 interface()
