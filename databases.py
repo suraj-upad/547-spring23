742b50c91f7d742e1a85a38a45d4bdbca8148aee
Suraj U <suraj.upad@gmail.com>
2023-02-01 12:42:20 -0500

    Able to get patient entry using mrn

--- databases.py
+++ databases.py
@@ -14,7 +14,14 @@ def main_driver():
     db.append(create_patient_entry('Loser Mcgee', 101, 69))
     db.append(create_patient_entry('Ben Hawk', 121, 23))
     db.append(create_patient_entry("'Mike Dover", 1032, 29))
-    print(db)
+    print(get_pat(101,db))
+
+def get_pat(mrn,db):
+    for p in db:
+        if p[1] == mrn:
+            return p
+    return False
+    
 
 if __name__ == "__main__":
     main_driver()
