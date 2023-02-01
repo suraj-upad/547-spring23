9743ee4684d4583326e365ee2ceeb0489a366089
Suraj U <suraj.upad@gmail.com>
2023-02-01 13:04:45 -0500

    added ability to get test result of specific test and patient

--- databases.py
+++ databases.py
@@ -16,7 +16,17 @@ def main_driver():
     db.append(create_patient_entry("'Mike Dover", 1032, 29))
     print(get_pat(101,db))
     update_tests(db,101,"HDL",120)
+    update_tests(db,121,"HDL",100)
+    update_tests(db,1032,"HDL",220)
+    
     print(get_pat(101,db))
+    room = ["102","34","13322"]
+    print_direc(db,room)
+    
+    print(get_test_result(db,101,'HDL'))
+    print(get_test_result(db,121,'HDL'))
+    print(get_test_result(db,1032,'HDL'))
+
 
 def get_pat(mrn,db):
     for p in db:
@@ -33,6 +43,17 @@ def update_tests(db, mrn, test_n, test_v):
     p[3].append([test_n, test_v])
     return
 
+def print_direc(db, room):
+    for i,p in enumerate(db):
+        print("patient {} is in room {}".format(p[0],room[i]))
+        
+def get_test_result(db, mrn, test_name):
+    p = get_pat(mrn,db)
+    for t in p[3]:
+        if t[0]==test_name:
+            return t[1]
+        
+    
 
 if __name__ == "__main__":
     main_driver()
