996ab2006e5fe6605b0744623bd752c8d5b63950
Suraj U <suraj.upad@gmail.com>
2023-02-01 12:25:11 -0500

    Can create a new patient entry with new function

---
+++ databases.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  1 12:17:32 2023
+
+@author: suraj
+"""
+
+def create_patient_entry(patient_name, patient_mrn, patient_age):
+    ls = [patient_name, patient_mrn, patient_age]
+    return ls
+
+def main_driver():
+    db=[]
+    db.append(create_patient_entry('Loser Mcgee', 101, 69))
+    db.append(create_patient_entry('Ben Hawk', 121, 23))
+    db.append(create_patient_entry("'Mike Dover", 1032, 29))
+    print(db)
+
+if __name__ == "__main__":
+    main_driver()
