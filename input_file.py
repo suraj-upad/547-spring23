70adadb535b072b9632d0213b00373192e246786
Suraj U <suraj.upad@gmail.com>
2023-02-15 12:41:01 -0500

    Lecture material

---
+++ input_file.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 15 12:24:15 2023
+
+@author: suraj
+"""
+
+def read_file(filename):
+    in_file = open("input_data.txt", "r")
+    #fruits = in_file.readlines()
+    #print(fruits)
+    patient_id = int(in_file.readline().strip("\n").split("=")[1])
+
+
+def test_read_file():
+    from ,,,
+    filename = "my_test"
+    
+    
+print(patient_id)
+for line in in_file:
+    print(line)
+    
+in_file.close()
